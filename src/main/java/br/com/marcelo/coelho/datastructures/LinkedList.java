4e1a91e8b7e975267e6a17aa4d18222a07578c04
marcelowender <marcelowender@hotmail.com>
2019-10-10 23:20:12 -0300

    Estudies and comments about linkedList

--- src/main/java/br/com/marcelo/coelho/datastructures/LinkedList.java
+++ src/main/java/br/com/marcelo/coelho/datastructures/LinkedList.java
@@ -1,5 +1,17 @@
 package br.com.marcelo.coelho.datastructures;
 
+
+/**
+ * Advantage:<br>
+ * Insert Element and Delete have the time of O(1)
+ * <p>
+ * Disadvantage: comparing with arrays, the linked list dont have random access,
+ * thats mean Get and Set with the time of O(n)
+ * <p>
+ * Can grow incrementally isn't necessary resize method
+ * <p>
+ * Ideal to use in Stacks or Queues
+ */
 public class LinkedList {
 
 
