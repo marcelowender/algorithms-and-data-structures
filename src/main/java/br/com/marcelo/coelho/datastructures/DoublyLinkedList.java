a98f131b32d5ff9f1752aa8bde28e2be7c7b2770
marcelowender <marcelowender@hotmail.com>
2019-10-31 23:07:32 -0300

    Methods to be implemented

--- src/main/java/br/com/marcelo/coelho/datastructures/DoublyLinkedList.java
+++ src/main/java/br/com/marcelo/coelho/datastructures/DoublyLinkedList.java
@@ -5,7 +5,7 @@ package br.com.marcelo.coelho.datastructures;
 /**
  * The intention here is understood what is the difference between DLL and SLL
  * When we can use DLL
- *
+ * <p>
  * Advantage:<br>
  * <p>
  * Disadvantage:
@@ -16,11 +16,69 @@ public class DoublyLinkedList<T> {
     private Node<T> head;
 
 
+    public void addBack(T value) {
+
+
+    }
+
+    public void addFront(T value) {
+
+    }
+
+    public void addValueIndex(T value, int index) {
+
+    }
+
+    public T getFirst() {
+        return null;
+    }
+
+
+    public T getLast() {
+        return null;
+    }
+
+
+    public void delete(T value) {
+
+    }
+
+
+    public int indexOf(T value) {
+        return 0;
+    }
+
+    public boolean contains(T value) {
+        return false;
+    }
+
+
+    public int getCount() {
+        return 0;
+    }
+
+
+    public void clear() {
+
+    }
+
+
+    public void print() {
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+
     private static class Node<T> {
         private T data;
         private Node next;
         private Node prev;
-
 
         public Node(T value) {
             this.data = value;
