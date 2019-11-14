81f84d71b4dc2b2668f89f32bdf3a4f565cee08d
marcelowender <marcelowender@hotmail.com>
2019-11-13 23:22:30 -0300

    Study concept of FIFO

---
+++ src/main/java/br/com/marcelo/coelho/datastructures/Queue.java
@@ -0,0 +1,12 @@
+package br.com.marcelo.coelho.datastructures;
+
+/**
+ * Queues work through the First In, First Out approach.
+ * For example:
+ * Condsider the Linkedlist  1->2->3->4->5
+ * The first element in this linkedList is 1 and the last element is 5
+ * The one way to remove elements in this list is removing the first element
+ * the implementation remove() remove one element from HEAD.
+ */
+public class Queue {
+}
